de6386338321fc1ec7703da2e2fd1a09c69d76a4
Levi Fry <levifry@Levis-MacBook-Air.local>
2022-06-06 15:45:11 -0700

    fixed port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import { createServer } from 'node:http';
 const host = 'localhost'
-const port = '3001'
+const port = '3000'
 const server = createServer((request, response) => {
 let path = request.url.split('/')
   response.end('Hello, Galvanize!');
